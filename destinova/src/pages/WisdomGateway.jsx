41ad0cf47aa464764dd61ac866e35d9fe6059e0c
Chamath Kaushalya <chamathkaushalya8@gmail.com>
2025-03-20 16:43:28 +0530

    make the academic networking

--- destinova/src/pages/WisdomGateway.jsx
+++ destinova/src/pages/WisdomGateway.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavigationBar from "../components/NavigationBar";
+import Footer from "../components/Footer";
 import styles from "./WisdomGateway.module.css";
 
 const WisdomGateway = () => {
@@ -9,54 +10,80 @@ const WisdomGateway = () => {
       <div className={styles.wisdom}>
         <div className="container d-flex justify-content-center pt-lg-5">
           <div className={styles.content}>
-            <div className="d-flex justify-content-between">
-              <div className="col-lg-4 border border-light border-2 text-center rounded p-3 me-1 py-4">
-                <h5>State Universities</h5>
+            <div className="d-lg-flex justify-content-between">
+              <a
+                href="https://www.ugc.ac.lk/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`col-lg-4 mt-1 p-3 me-1 border border-light border-2 rounded text-center ${styles.education} text-decoration-none text-reset`}
+              >
+                <h4>State Universities</h4>
                 <p className="small text-">
                   Sri Lanka's state universities, managed by the UGC, provide
                   free higher education based on GCE A/L results, fostering
                   research and development.
                 </p>
-              </div>
-              <div className="col-lg-4 border border-light border-2 text-center rounded p-3 me-1">
-                <h5>Non State Universities</h5>
+              </a>
+              <a
+                href="https://www.coursenet.lk/courses/?levels=1,2"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`col-lg-4 mt-1 p-3 me-1 border border-light border-2 rounded text-center ${styles.education} text-decoration-none text-reset`}
+              >
+                <h4>Non State Universities</h4>
                 <p className="small text-">
                   Non-state universities in Sri Lanka are privately funded
                   institutions offering higher education in various fields.
                   Notable ones include SLIIT, NSBM, CINEC, and APIIT, providing
                   alternatives to state universities.
                 </p>
-              </div>
-              <div className="col-lg-4 border border-light border-2 text-center rounded p-3">
-                <h5>Diploma and Higher Diploma</h5>
+              </a>
+              <a
+                href="https://www.coursenet.lk/courses/?levels=3,4,5,6"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`col-lg-4 mt-1 p-3 me-1 border border-light border-2 rounded text-center ${styles.education} text-decoration-none text-reset`}
+              >
+                <h4>Diploma and Higher Diploma</h4>
                 <p className="small text-">
                   A diploma is a short-term program offering foundational
                   skills, while a higher diploma provides advanced knowledge,
                   often between a diploma and degree.
                 </p>
-              </div>
+              </a>
             </div>
-            <div className="d-flex justify-content-center mt-1">
-              <div className="col-lg-4 border border-light border-2 text-center rounded p-3 me-1 py-4">
-                <h5>NVQ Courses</h5>
+            <div className="d-lg-flex justify-content-center">
+              <a
+                href="https://course.vta.lk/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`col-lg-4 mt-1 p-3 me-1 border border-light border-2 rounded text-center ${styles.education} text-decoration-none text-reset`}
+              >
+                <h4>NVQ Courses</h4>
                 <p className="small text-">
                   The NVQ (National Vocational Qualification) program in Sri
                   Lanka offers practical, job-oriented training and
                   certification in various fields, ensuring individuals are
                   skilled and employable.
                 </p>
-              </div>
-              <div className="col-lg-4 border border-light border-2 text-center rounded p-3 me-1">
-                <h5>Certificate Programs</h5>
+              </a>
+              <a
+                href="https://www.coursenet.lk/courses/?levels=7"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`col-lg-4 mt-1 p-3 me-1 border border-light border-2 rounded text-center ${styles.education} text-decoration-none text-reset`}
+              >
+                <h4>Certificate Programs</h4>
                 <p className="small text-">
                   A certificate is a short qualification that confirms
                   completion of a specific course or training, typically taking
                   a few months.
                 </p>
-              </div>
+              </a>
             </div>
           </div>
         </div>
+        <Footer />
       </div>
     </>
   );
